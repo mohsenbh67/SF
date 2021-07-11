8ae1d9facd2717b95e18d35cbe30fcec48a7671d
Mohsen <behroozi.mohsen1@gmail.com>
2021-07-11 17:09:10 +0430

    Css changes 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="antialiased">
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
