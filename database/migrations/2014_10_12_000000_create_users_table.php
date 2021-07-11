fe92f621dbc5f191cf346d16e96c2cb5d5c92e2c
Mohsen <behroozi.mohsen1@gmail.com>
2021-07-11 14:29:54 +0430

    Migration started

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 class CreateUsersTable extends Migration
 {
@@ -19,11 +20,20 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->set('role', ['user','admin','shop'])->default('user');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'icpherking@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin'),
+            'role' => 'admin',
+        ]);
     }
 
     /**
